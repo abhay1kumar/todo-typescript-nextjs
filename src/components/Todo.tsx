cadab9fba8b4934a9d1ea5f8a1603a278ec84b8c
Abhay Kumar <abhay4waytechnologies@gmail.com>
2023-07-20 20:55:49 +0530

    done

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -6,6 +6,7 @@ import TodoItem from './TodoItem';
 type Todo = {
   id: string,
   message: string,
+  time:string,
   isDone: boolean
 }
 
@@ -14,6 +15,7 @@ const Todo = () => {
   const id = uuidv4()
   const [todos, setTodos] = useState<Todo[]>([])
   const [text, setText] = useState('')
+  const [time, setTime] = useState("")
 
   const onChangeHandler = (value: string) => {
     setText(value)
@@ -26,15 +28,18 @@ const Todo = () => {
         {
           id: id,
           message: text,
+          time:time,
           isDone: false
         }
       ]
     )
     setText("")
+    setTime("")
   }
 
 
   const handleToggleTodo = (id: string) => {
+
     setTodos(prevTodos =>
       prevTodos.map(todo =>
         todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
@@ -42,15 +47,41 @@ const Todo = () => {
     );
   };
 
+  const onChangeTimeHandler = (value: string) => {
+    const isValidNumber = /^\d{0,2}$/.test(value);
+
+    if (value.length <= 2 && isValidNumber) {
+   
+      return setTime(value)
+    }
+  }
+
 
   return (
     <div>
       <div className='p-4 flex gap-2'>
-        <input value={text} onChange={(e) => onChangeHandler(e.target.value)} className='border border-gray-400 rounded p-2' type='text' placeholder='type here...' />
-        <button disabled={text.length < 1 ? true : false} className="border p-1 rounded text-white bg-blue-300 border-gray-400" onClick={() => addTodo()}>Add Todo</button>
+        <input value={text} onChange={(e) => onChangeHandler(e.target.value)}
+          className='border border-gray-400 rounded p-2'
+          type='text'
+          placeholder='type here...' />
+        <div className='flex flex-col justify-center items-center'>
+          <label>Time (minute)</label>
+          <input value={time} onChange={(e) => onChangeTimeHandler(e.target.value)}
+          className='border border-gray-400 rounded p-2 w-10'
+          type='text'
+          placeholder='00' />
+        </div>
+       
+
+        <button
+          disabled={text.length < 1 ? true : false}
+          className="border p-1 rounded text-white bg-blue-300 border-gray-400"
+          onClick={() => addTodo()}
+        >Add Todo
+        </button>
       </div>
 
-      <TodoItem todos={todos} handleToggleTodo={handleToggleTodo}/>
+      <TodoItem todos={todos} handleToggleTodo={handleToggleTodo} />
     </div>
   )
 }
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 interface Todo {
   id: string;
   message: string;
+  time: string;
   isDone: boolean;
 }
 
@@ -12,13 +13,46 @@ interface TodosProps {
   handleToggleTodo: (id: string) => void;
 }
 
+interface TimerProps {
+  initialTime: number;
+}
+
+const Timer = ({ initialTime }: TimerProps) => {
+  const [remainingTime, setRemainingTime] = useState<number>(initialTime * 60);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setRemainingTime(prevTime => prevTime - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const formatTime = (timeInSeconds: number) => {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = timeInSeconds % 60;
+    const formattedMinutes = minutes.toString().padStart(2, '0');
+    const formattedSeconds = seconds.toString().padStart(2, '0');
+    return `${formattedMinutes}:${formattedSeconds}`;
+  };
+
+  return <span>{remainingTime > 0 ? formatTime(remainingTime) : '00:00'}</span>;
+};
+
 const TodoItem = ({ todos, handleToggleTodo }: TodosProps) => {
   return (
     <>
       {todos && todos.map((val, index) => {
         const statusClass = val.isDone ? 'bg-green-600' : 'bg-yellow-500';
+        const stringValue: string = val.time;
+        const numberValue: number = parseInt(stringValue);
         return <div className=' mt-2 flex gap-2 p-1 border border-grey-400 max-w-md justify-between' key={index}>
           <textarea readOnly value={val.message} className='p-1 font-medium ' />
+          {
+            !val.isDone && <Timer initialTime={numberValue} />
+          }
           <button onClick={() => handleToggleTodo(val.id)}><span className={`p-2 rounded ${statusClass}`}> {val.isDone ? "Completed" : "Pending"}</span></button>
         </div>
       })}
